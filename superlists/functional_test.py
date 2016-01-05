3296aede09609ed8fb74a1932e4f2843e1be8fcb
Vickie Victor <victo22v@mtholyoke.edu>
2016-01-05 14:28:58 -0500

    Able to save a single to-do item.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -12,8 +12,6 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-
-
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage.
         self.browser.get('http://localhost:8000')
@@ -37,12 +35,11 @@ class NewVisitorTest(unittest.TestCase):
         #When she hits enter, the page updates, and now the page lists
         # "1. Buy peacock feathers" as an item in a to-do lists
         inputbox.send_keys(Keys.ENTER)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1. Buy peacock feathers' for row in rows),
-            "Input is not in a table"
-        )
+        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])
+
 
         #There is still a text box inviting her to add another item
         #She enters 'Use peacock feathers to make fly'
