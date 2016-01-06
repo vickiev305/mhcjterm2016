fa287ce765c867d0e1e55f8f5e35e5c88bca34bf
Vickie Victor <victo22v@mtholyoke.edu>
2016-01-06 10:46:07 -0500

    Added a first model for Item.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,6 +3,8 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from lists.views import home_page
 from django.http import HttpRequest
+from lists.models import Item
+
 
 from django.test import Client
 
@@ -33,3 +35,22 @@ class HomePageTest(TestCase):
             'home.html',{'new_item_text':'A new list item'}
         )
         self.assertEqual(response.content.decode(),expected_html)
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item =saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'The second item')
