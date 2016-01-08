f122e1901abf279bd9bc794dbc226cc910a7bff1
Vickie Victor <victo22v@mtholyoke.edu>
2016-01-08 11:44:24 -0500

    The entire thing works

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -38,24 +38,60 @@ class NewListTest(TestCase):
             data={'item_text':'A new lists item'}
         )
 
-        self.assertRedirects(response,'/lists/the-only-list/')
+        new_list = List.objects.first()
+        self.assertRedirects(response,'/lists/%d/' % (new_list.id,))
+
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data = {'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data = {'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id))
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list/')
+        new_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (new_list.id,))
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
+    def test_displays_only_items_for_list(self):
         new_list = List.objects.create()
         Item.objects.create(text = 'itemey 1', list = new_list)
         Item.objects.create(text = 'itemey 2', list = new_list)
+        other_list = List.objects.create()
+        Item.objects.create(text = 'other item 1', list = other_list)
+        Item.objects.create(text = 'other item 2', list = other_list)
 
-        response = self.client.get('/lists/the-only-list/')
+        response = self.client.get('/lists/%d/' % (new_list.id,))
 
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
+        self.assertNotContains(response, 'other item 1')
+        self.assertNotContains(response, 'other item 2')
 
-class ItemandListModelsTest(TestCase):
+    def test_passes_correct_list_to_template(self):
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
+
+class ItemAndListModelsTest(TestCase):
     def test_saving_and_retrieving_items_in_list(self):
         new_list=List()
         new_list.save()
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -8,8 +8,14 @@ def home_page(request):
 def new_list(request):
     new_list = List.objects.create()
     Item.objects.create(text = request.POST['item_text'], list = new_list)
-    return redirect('/lists/the-only-list/')
+    return redirect('/lists/%d/' % (new_list.id,))
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request,'list.html',{'items': items,})
+def view_list(request, list_id):
+    list_ = List.objects.get(id = list_id)
+    items = Item.objects.filter(list = list_)
+    return render(request,'list.html',{'items': items,'list': list_})
+
+def add_item(request, list_id):
+    list_ = List.objects.get( id = list_id)
+    Item.objects.create( text = request.POST['item_text'], list = list_)
+    return redirect('/lists/%d/' % (list_.id))
