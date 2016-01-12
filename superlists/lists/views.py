cd3aaf2e3a5e1b3403c282a35a5f1bcaa81b9780
Vickie Victor <victo22v@mtholyoke.edu>
2016-01-12 14:18:10 -0500

    Refactor list view to handle adding

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -20,10 +20,10 @@ def new_list(request):
     return redirect('/lists/%d/' % (new_list.id,))
 
 def view_list(request, list_id):
-    list_ = List.objects.get(id = list_id)
-    return render(request,'list.html',{'list': list_})
 
-def add_item(request, list_id):
-    list_ = List.objects.get( id = list_id)
-    Item.objects.create( text = request.POST['item_text'], list = list_)
-    return redirect('/lists/%d/' % (list_.id))
+    list_ = List.objects.get(id = list_id)
+
+    if request.method =='POST':
+        Item.objects.create( text = request.POST['item_text'], list = list_)
+
+    return render(request,'list.html',{'list': list_})
